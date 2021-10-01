b0abba6994e96a1a91a7558fa7a5134fa3dfcb32
JAY PANDIT <jaypandit@JAYs-MacBook-Pro.local>
2021-09-30 17:53:59 -0700

    New edited file

--- src/main/java/csc131/GiftCardApplication.java
+++ src/main/java/csc131/GiftCardApplication.java
@@ -1,2 +1,25 @@
-package csc131;public class GiftCardApplication {
+package csc131;
+
+import java.util.ArrayList;
+
+public class GiftCardApplication {
+    public static void main(String[]args)
+    {
+        GiftCard gc1 = new GiftCard(25,125.00);
+        GiftCard gc2 = new GiftCard(50, 150.50);
+        GiftCard gc3 = new GiftCard(30, 135.50);
+
+        ArrayList<GiftCard> giftCards = new ArrayList<GiftCard>();
+        giftCards.add(gc1);
+        giftCards.add(gc2);
+        giftCards.add(gc3);
+
+        int gcLength = giftCards.size();
+        for(int i=0;i<gcLength;i++)
+        {
+            GiftCard gc = giftCards.get(i);
+            System.out.println("GiftCard "+gc.getId() + " from store# "+gc.getIssuingStore() + " has a balance of "+gc.getBalance());
+
+        }
+    }
 }
